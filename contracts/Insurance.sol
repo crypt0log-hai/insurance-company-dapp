9ee6d54b8d06f4ced453fe20650dc86f16f276e4
Srdjenovic Luca <luca.srdjenov@he-arc.ch>
2018-11-20 01:24:53 +0100

    Add front-end client and bill

--- contracts/Insurance.sol
+++ contracts/Insurance.sol
@@ -2,10 +2,40 @@ pragma solidity ^0.4.24;
 
 contract Insurance {
 
-    string public name;
+    event NewBill(uint billId, string name, uint cost, bool isPayed);
 
-    //Constructor
+    struct Client {
+        string name;
+        uint franchise;
+        uint count; //Decompte
+    }
+
+    struct Bill {
+        string name;
+        uint cost;
+        bool isPayed;
+    }
+
+    mapping (uint => address) public billToOwner;
+    mapping (address => Client) public clients;
+    mapping (address => uint) public ownerBillCount;
+
+    Bill[] public bills;
+
     constructor() public {
-      name = "Helvetia";
+        createClient("Luca Srdjenovic", 10);
+        createBill("Dentiste", 2);
+        createBill("Medecin", 4);
+    }
+
+    function createClient(string _name, uint _franchise) private {
+        clients[msg.sender] = Client(_name, _franchise, 0);
+    }
+
+    function createBill(string _name, uint _cost) private {
+        uint id = bills.push(Bill(_name, _cost, false)) - 1;
+        billToOwner[id] = msg.sender;
+        ownerBillCount[msg.sender]++;
+        emit NewBill(id, _name, _cost, false);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,94 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON("Insurance.json", function(insurance) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Insurance = TruffleContract(insurance);
+      // Connect provider to interact with contract
+      App.contracts.Insurance.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      return App.render();
+    });
+  },
+
+  render: function() {
+    var insuranceInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    //Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your account -> " + account);
+      }
+    });
+
+    //Load contract client data
+    App.contracts.Insurance.deployed().then(function(instance) {
+      insuranceInstance = instance;
+      return insuranceInstance.clients(App.account);
+    }).then(function(client) {
+      var clientResult = $("#clientResult");
+      clientResult.empty();
+      console.log(client[1]);
+
+      var name = client[0];
+      var franchise = client[1];
+      var count = client[2];
+      var clientTemplate = "<tr><th>" + name + "</th><td>" + franchise + "</td><td>" + count + "</td></tr>";
+
+      clientResult.append(clientTemplate);
+    });
+
+    //Load contract bil data
+    App.contracts.Insurance.deployed().then(function(instance) {
+      insuranceInstance = instance;
+      return insuranceInstance.ownerBillCount(App.account);
+    }).then(function(billCount) {
+      var billResults = $("#billResults");
+      billResults.empty();
+
+      for (var i = 0; i < billCount; i++) {
+        insuranceInstance.bills(i).then(function(bill){
+          var temp = i-1;
+          var id = temp;
+          var name = bill[0];
+          var cost = bill[1];
+          var isPayed = bill[2];
+
+          //Render candidate billResults
+          var billTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + cost + "</td><td>" + isPayed + "</td></tr>";
+          billResults.append(billTemplate);
+        });
+      }
+
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
   },
 
   bindEvents: function() {
